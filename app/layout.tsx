3a21eb20d9f72ac74d8f52a3cf1d4e4d7839366d
crackerky <crackert.ky@gmail.com>
2025-05-05 09:23:15 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export const metadata: Metadata = {
       follow: true,
     },
   },
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
 };
 
 export default function RootLayout({
